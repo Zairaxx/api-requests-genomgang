5d7741a835f3fc1e753488fab6eaf606ebe44912
Zairaxx <brandontsegai@gmail.com>
2025-03-25 10:40:43 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,68 @@
+//POST
+
+const postData = async (URL, data) => {
+    let response = await axios.post(URL, data) 
+    console.log(response);
+}
+
+document.querySelector("#addUser").addEventListener("click",
+    () => {
+        let newUser = {
+            name: "Kalle Anka"
+        };
+
+        postData("http://localhost:5001/users", newUser)
+    })
+//DELETE
+
+const deleteUser = async (url) => {
+    await axios.delete(url)
+}
+
+//PUT
+
+const updateUser = async (url,data) => {
+    let response = await axios.put(url, data);
+    console.log(response);
+}
+
+
+//render function
+const getData = async (URL) => {
+    let response = await axios.get(URL);
+    console.log(response.data);
+    return response.data
+}
+
+let renderPage = async () => {
+    let users = await getData("http://localhost:5001/users");
+    users.forEach(user => {
+        let div = document.createElement("div");
+        div.innerHTML = `<h2>Name: ${user.name} - id: ${user.id}</h2>`
+        //DELETE
+        let deleteBtn = document.createElement("button");
+        deleteBtn.addEventListener("click", () => {deleteUser("http://localhost:5001/users/" + user.id) })
+        deleteBtn.textContent = "Delete user"
+        div.append(deleteBtn);
+        document.body.append(div);
+
+        //UPDATE
+        let updateBtn = document.createElement("button");
+        
+        let updatedUser = {
+            name:"Kajsa Anka"
+        }
+
+        updateBtn.addEventListener("click", () => {updateUser(`http://localhost:5001/users/${user.id}`, updatedUser) })
+        updateBtn.textContent = "Update user"
+        div.append(updateBtn);
+        document.body.append(div);
+    })
+    
+}
+
+renderPage();
+
+//Http request
+
+//GET
---
+++ readme.md
@@ -0,0 +1,7 @@
+Kör följande kommando i terminalen:
+
+npm install -g json-server
+
+För att skapa en lokal json server, öppna upp terminalen i ditt projekt och kör följande kommando:
+
+json-server db.json --port 5001
